0a874447cfb35b58c794f3af0ff14579487d2437
Elvis Nuñez <elvisnunez@me.com>
2015-03-11 18:10:00 +0100

    Clean up tests

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -1,5 +1,7 @@
 @import XCTest;
+
 #import "Note.h"
+
 #import "NSArray+HYPFind.h"
 
 @interface Tests : XCTestCase
@@ -8,38 +10,26 @@
 
 @implementation Tests
 
-- (void)setUp
-{
-    [super setUp];
-
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-
-    [super tearDown];
-}
-
 - (void)testFindObjectWithRemoteID
 {
-    NSArray *array = [self makeNotes];
+    NSArray *array = [self notes];
     Note *note = [array findObjectWithRemoteID:2];
     XCTAssertEqual(note.remoteID, @(2));
 }
 
 #pragma mark - Helpers
 
-- (NSArray *)makeNotes
+- (NSArray *)notes
 {
     NSMutableArray *notes = [NSMutableArray new];
+
     for (int i = 0; i < 10; ++i) {
         Note *note = [Note new];
         note.remoteID = @(i);
         note.note = [NSString stringWithFormat:@"None %d", i];
         [notes addObject:note];
     }
+
     return notes;
 }
 
