605f436d92cc328ca9f04c4e3fde70eb251cac20
Elvis Nuñez <3lvis@users.noreply.github.com>
2015-05-13 11:05:45 +0200

    Update NSArray+HYPFind.m

--- Source/NSArray+HYPFind.m
+++ Source/NSArray+HYPFind.m
@@ -2,13 +2,11 @@
 
 @implementation NSArray (HYPFind)
 
-- (id)objectWhereRemoteID:(id)remoteID
-{
+- (id)objectWhereRemoteID:(id)remoteID {
     return [self objectWhereAttribute:@"remoteID" isEqualTo:remoteID];
 }
 
-- (id)objectWhereAttribute:(NSString *)attribute isEqualTo:(id)comparedAttribute;
-{
+- (id)objectWhereAttribute:(NSString *)attribute isEqualTo:(id)comparedAttribute; {
     return [[self filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"%K == %@", attribute, comparedAttribute]] firstObject];
 }
 
